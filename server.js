57bd9fd9c8324544016c12ed18a207c08f951252
Hieu Nguyen <hieu@happysignals.com>
2022-06-26 01:09:11 +0300

    Fix client-side routing

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "build")));
 
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.listen(port, () => console.log(`App is live on port ${port}!`));
